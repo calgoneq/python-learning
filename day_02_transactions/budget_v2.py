349d8f5cc3a96e07ba8f70ef7334d15711935b6c
calgoneq <calgoneq@gmail.com>
2026-04-21 16:47:45 +0200

    refactor: changed how i print items

--- day_02_transactions/budget_v2.py
+++ day_02_transactions/budget_v2.py
@@ -28,38 +28,38 @@ Saldo startowe: {saldo:.2f} zł\n
 
 for i, item in enumerate(transakcje, start=1):
     kategorie_suma[item["kategoria"]] = kategorie_suma.get(item["kategoria"], 0.0) + item["kwota"]
-    transaction = f"{i:>3}. {item['data']:>3} | {item['sklep']:<15} | {item['kwota']:>7.2f} zł | {item['kategoria']}"
+    transaction = f"{i:>3}. {item['data']} | {item['sklep']:<15} | {item['kwota']:>7.2f} zł | {item['kategoria']}"
     print(transaction)
 
-wydane = float(f"{sum(kategorie_suma.values())}")
-saldo_po = saldo - wydane
+wydane: float = sum(kategorie_suma.values())
+saldo_po: float = saldo - wydane
 
 print(f"""{line_thin}
-📊 Podsumowanie kategorii:
-  {f"jedzenie":<10} : {kategorie_suma.get("jedzenie", 0.0):>7.2f} zł
-  {f"styl":<10} : {kategorie_suma.get("styl", 0.0):>7.2f} zł 
-  {f"zdrowie":<10} : {kategorie_suma.get("zdrowie", 0.0):>7.2f} zł\n
-💰 Saldo po wydatkach: {saldo_po:.2f} zł
+📊 Podsumowanie kategorii:""")
+for key, value in kategorie_suma.items():
+    print(f"{key:<10}:{value:>7.2f} zł")
+
+print(f"""
+💸 {"Łącznie wydane:":<20}{wydane:.2f} zł
+💰 {"Saldo po wydatkach: ":<20}{saldo_po:.2f} zł
 {line_thin}""")
 
 for item in planowane:
     plany_suma[item["opis"]] = plany_suma.get(item["opis"], 0.0) + item["kwota"]
 
-zaplanowane = float(f"{sum(plany_suma.values())}")
+zaplanowane: float = sum(plany_suma.values())
 
 print(f"🎯 Zobowiązania ({zaplanowane:.2f} zł):")
 
 for i, item in enumerate(planowane, start=1):
-    plany_suma[item["opis"]] = plany_suma.get(item["opis"], 0.0) + item["kwota"]
     planned = f"- {item['opis']:<17} | {item['kwota']:>10.2f} zł | do {item['deadline']}"
     print(planned)
 
-dni_danych = 3
-wydano = wydane + zaplanowane
-avg = wydano / dni_danych
-msc_burn = avg * 30
+dni_danych: int = 3
+avg: float = wydane / dni_danych
+msc_burn: float = avg * 30
 
-runway_miesiace = 0
+runway_miesiace: int = 0
 
 while saldo > 0:
     saldo -= msc_burn
@@ -68,7 +68,6 @@ while saldo > 0:
 print(f"""
 💰 Saldo po zobowiązaniach: {saldo_po - zaplanowane:.2f} zł
 {line_thin}
-💸 Łącznie wydane:  {wydano:.2f} zł
 
 🔮 Prognoza runway (avg {dni_danych} dni):
 Dzienna średnia: {avg:.2f} zł
