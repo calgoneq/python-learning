152a8fec90e4ae312f1101f97863f6ae39c04140
calgoneq <calgoneq@gmail.com>
2026-05-15 17:51:05 +0200

    test: path, query params and error handling tests

--- day_12_fastapi_ext/tests/test_api.py
+++ day_12_fastapi_ext/tests/test_api.py
@@ -51,3 +51,98 @@ def test_get_after_new_transaction_returns_correct_json():
 
     assert response.status_code == 200
     assert any(t["sklep"] == "test_sklep" for t in data)
+
+def test_get_with_kategoria_parameter_returns_correct_data():
+    new_transactions = {
+        "sklep": "test_sklep",
+        "kwota": 10.2,
+        "kategoria": "jedzenie",
+        "data": "2026-02-12",
+    }
+    client.post("/transactions", json=new_transactions)
+
+    response = client.get("/transactions?kategoria=jedzenie")
+    data = response.json()
+    assert response.status_code == 200
+    assert any(t["kategoria"] == "jedzenie" for t in data)
+
+def test_get_transaction_with_id_returns_only_transaction_with_correct_id():
+    transaction_1: dict = {
+        "sklep": "domino",
+        "kwota": 18.5,
+        "kategoria": "psy",
+        "data": "2026-02-18",
+    }
+
+    transaction_2: dict = {
+        "sklep": "niewiem",
+        "kwota": 28.4,
+        "kategoria": "zwierzęta",
+        "data": "2026-02-15",
+    }
+
+    transaction_3: dict = {
+        "sklep": "loco",
+        "kwota": 12.7,
+        "kategoria": "ludzie",
+        "data": "2026-02-22",
+    }
+
+    client.post("/transactions", json=transaction_1)
+    client.post("/transactions", json=transaction_2)
+    client.post("/transactions", json=transaction_3)
+
+    transaction_id = 1
+    response = client.get(f"/transactions/{transaction_id}")
+    data = response.json()
+
+    assert response.status_code == 200
+    assert "zwierzęta" in data["kategoria"]
+
+def test_get_transaction_with_incorrect_id_returns_404():
+    transaction_id = 200
+    response = client.get(f"/transactions/{transaction_id}")
+
+    assert response.status_code == 404
+    data = response.json()
+    assert str(transaction_id) in data["detail"]
+
+def test_delete_transaction_removes_and_returns_200():
+    transaction_to_delete = 1
+
+    transaction_1: dict = {
+        "sklep": "domino",
+        "kwota": 18.5,
+        "kategoria": "psy",
+        "data": "2026-02-18",
+    }
+
+    transaction_2: dict = {
+        "sklep": "niewiem",
+        "kwota": 28.4,
+        "kategoria": "zwierzęta",
+        "data": "2026-02-15",
+    }
+
+    transaction_3: dict = {
+        "sklep": "loco",
+        "kwota": 12.7,
+        "kategoria": "ludzie",
+        "data": "2026-02-22",
+    }
+
+    client.post("/transactions", json=transaction_1)
+    client.post("/transactions", json=transaction_2)
+    client.post("/transactions", json=transaction_3)
+
+    before_delete = client.get('/transactions')
+    before_data = before_delete.json()
+
+    response = client.delete(f"/transactions/{transaction_to_delete}")
+
+    assert response.status_code == 200
+
+    after_delete = client.get('/transactions')
+    after_data = after_delete.json()
+
+    assert len(before_data) > len(after_data)
