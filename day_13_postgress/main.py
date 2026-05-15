350bf093445616f843ed79135c31e570ccc039d3
calgoneq <calgoneq@gmail.com>
2026-05-15 19:37:11 +0200

    feat: connect main.py to db.py, replace old storage

---
+++ day_13_postgress/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, HTTPException
+from contextlib import asynccontextmanager
+
+from exceptions import ValidationError
+from transaction import Transaction
+from models import TransactionIn
+from db import init_db, get_all_transactions, get_transaction_by_id, add_transaction, remove_transaction
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+app = FastAPI()
+
+@app.get("/")
+def get_status():
+    return {"message": "server running"}
+
+@app.get("/transactions")
+def get_transactions(kategoria: str = None):
+    transactions = get_all_transactions(kategoria)
+    return transactions
+
+@app.get("/transactions/{transaction_id}")
+def get_transactions_by_id(transaction_id: int):
+    transaction = get_transaction_by_id(transaction_id)
+    if transaction == None:
+        raise HTTPException(status_code=404, detail=f"Transakcja o id {transaction_id} nie istnieje")
+    else: 
+        return transaction
+
+@app.post("/transactions", status_code=201)
+def post_transaction(item: TransactionIn):
+    try:
+        transaction = Transaction(item.sklep, item.kwota, item.kategoria, item.data)
+        data = transaction.to_dict()
+        add_transaction(data)
+        return {"message": "ok", "transaction": data}
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=str(e))
+
+@app.delete("/transactions/{transaction_id}")
+def delete_transactions(transaction_id: int):
+    if remove_transaction(transaction_id):
+        return {"message": f"Usunięto transakcje o id {transaction_id}"}
+    else:
+        raise HTTPException(status_code=404, detail=f"Transakcja o id {transaction_id} nie istnieje")
