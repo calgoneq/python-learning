40f8a421ebedfd9561fe1fbd9d4e6a39e7c51741
calgoneq <calgoneq@gmail.com>
2026-05-16 14:30:19 +0200

    feat: rewrite db.py to support SQLAlchemy ORM

--- day_14_sqlalchemy/db.py
+++ day_14_sqlalchemy/db.py
@@ -1,71 +1,56 @@
-import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
-DB_CONFIG = {
-    "host": "localhost",
-    "port": 5432,
-    "dbname": "budget_tracker",
-    "user": "calgoneq",
-    "password": ""
-}
+from models_db import Base, TransactionModel
+
+DATABASE_URL = "postgresql://calgoneq@localhost/budget_tracker"
+engine = create_engine(DATABASE_URL)
 
 def init_db() -> None:
-    with psycopg2.connect(**DB_CONFIG) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS transactions (
-                id      SERIAL PRIMARY KEY,
-                sklep   TEXT NOT NULL,
-                kwota   FLOAT NOT NULL,
-                kategoria TEXT NOT NULL,
-                data    TEXT NOT NULL)
-            """);
+    with Session(engine) as session:
+        Base.metadata.create_all(engine)
+        session.commit()
 
 def get_all_transactions(kategoria: str = None) -> list[dict]:
-    with psycopg2.connect(**DB_CONFIG) as conn:
-        with conn.cursor() as cursor:
-            keys: tuple = ("id", "sklep", "kwota", "kategoria", "data")
-            
-            if kategoria is not None:
-                cursor.execute("SELECT * FROM transactions WHERE kategoria = %s", (kategoria,))
-            else:
-                cursor.execute("SELECT * FROM transactions")
+    with Session(engine) as session:
+        if kategoria is not None:
+            rows = session.query(TransactionModel).filter_by(kategoria=kategoria).all()
+        else:
+            rows = session.query(TransactionModel).all()
 
-            rows = cursor.fetchall()
-            list_of_dicts: list[dict] = [dict(zip(keys, row)) for row in rows]
+        list_of_dicts: list[dict] = [{"id": item.id, "sklep": item.sklep, "kwota": item.kwota, "kategoria": item.kategoria, "data": item.data} for item in rows]
+        session.commit()
 
-            return list_of_dicts
+        return list_of_dicts
         
 def get_transaction_by_id(transaction_id: int) -> dict | None:
-    with psycopg2.connect(**DB_CONFIG) as conn:
-        with conn.cursor() as cursor:
-            keys: tuple = ("id", "sklep", "kwota", "kategoria", "data")
-            cursor.execute("SELECT * FROM transactions WHERE id = %s", (transaction_id,))
-            row = cursor.fetchone()
+    with Session(engine) as session:
+        row = session.get(TransactionModel, transaction_id)
 
-            if row is None:
-                return None
-            response: dict = dict(zip(keys, row))
-            
-            return response
+        if row is None:
+            return None
+        
+        response: dict = {"id": row.id, "sklep": row.sklep, "kwota": row.kwota, "kategoria": row.kategoria, "data": row.data}
+        session.commit()
+        
+        return response
 
 def add_transaction(transaction: dict) -> dict:
-    with psycopg2.connect(**DB_CONFIG) as conn:
-        with conn.cursor() as cursor:
-            keys: tuple = ("id", "sklep", "kwota", "kategoria", "data")
-            cursor.execute(
-                "INSERT INTO transactions (sklep, kwota, kategoria, data) VALUES (%s, %s, %s, %s) RETURNING id, sklep, kwota, kategoria, data",
-                (transaction["sklep"], transaction["kwota"], transaction["kategoria"], transaction["data"])
-            )
-            row = cursor.fetchone()
-            response: dict = dict(zip(keys, row))
-
-            return response
+    with Session(engine) as session:
+        new_t = TransactionModel(sklep=transaction["sklep"], kwota=transaction["kwota"], kategoria=transaction["kategoria"], data=transaction["data"])
+        session.add(new_t)
+        session.commit()
+        session.refresh(new_t)
+        response: dict = {"id": new_t.id, "sklep": new_t.sklep, "kwota": new_t.kwota, "kategoria": new_t.kategoria, "data": new_t.data}
+        
+        return response
 
 def remove_transaction(transaction_id: int) -> bool:
-     with psycopg2.connect(**DB_CONFIG) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("DELETE FROM transactions WHERE id = %s", (transaction_id,))
-            if cursor.rowcount > 0:
-                return True
-            else:
-                return False
+     with Session(engine) as session:
+        obj = session.get(TransactionModel, transaction_id)
+        if obj:
+            session.delete(obj)
+            session.commit()
+            return True
+        else:
+            return False
